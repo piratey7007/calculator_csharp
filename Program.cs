17537a18d58ffbce4ad6fd0cd0584a3872f9aae5
Piratey7007 <piratey7007@runbox.com>
2023-05-05 12:02:41 -0500

    Calculator works without parentheses

--- Program.cs
+++ Program.cs
@@ -21,21 +21,31 @@ Result handleInput(string input)
         return new Result(1, "Invalid input. Please enter an operation.", null);
     }
 
-    Array characters = input.Split("");
+    Array characters = input.ToCharArray();
     List<object> parts = new List<object>();
     string? prevType = null;
     StringBuilder number = new StringBuilder();
 
-    for (int i = 0; i < characters.Length; i++)
+    for (int i = 0; i < characters.Length + 1; i++)
     {
+        if (i == characters.Length)
+        {
+            double finalNumber;
+            if (number.Length != 0)
+            {
+                finalNumber = Convert.ToDouble(number.ToString());
+                parts.Add(finalNumber);
+            }
+            break;
+        }
         char character = (char)characters.GetValue(i)!;
         if (character >= '0' && character <= '9')
         {
-            if (prevType == "number")
+            if (prevType != "number")
             {
                 number = new StringBuilder();
-                number.Append(character);
             }
+            number.Append(character);
             prevType = "number";
         }
         else if (character == '+' || character == '-' || character == '*' || character == '/')
@@ -47,16 +57,22 @@ Result handleInput(string input)
                 {
                     number = new StringBuilder();
                     number.Append(character);
+                    prevType = "number";
                 }
                 else
                 {
-                    return new Result(1, "Invalid operation. Please enter a valid operation.", null);
+                    return new Result(1, $"Invalid operation at character {i + 1}. Please enter a valid operation.", null);
                 }
-                prevType = "number";
             }
             else
             {
-                parts.Append(character);
+                double finalNumber;
+                if (number.Length != 0)
+                {
+                    finalNumber = Convert.ToDouble(number.ToString());
+                    parts.Add(finalNumber);
+                }
+                parts.Add(character);
                 prevType = "operation";
             }
         }
@@ -70,22 +86,92 @@ Result handleInput(string input)
         }
         else
         {
-            Console.WriteLine("Invalid character.");
-
-            return new Result(1, "Invalid character.", null);
+            return new Result(1, $"Invalid character at character {i + 1}.", null);
         }
     }
+    Console.WriteLine("_________________");
+    foreach (var part in parts) Console.WriteLine(part);
+    Console.WriteLine("_________________");
     return new Result(0, null, parts);
 }
 
 Result processParts(List<object> parts)
 {
-    return new Result(0, null, parts);
+    for (int i = 0; i < parts.Count; i++)
+    {
+        if (parts[i] is char operation)
+        {
+            if (i == 0 || i == parts.Count - 1)
+            {
+                return new Result(1, $"Invalid order of operations at character {i + 1}", null);
+            }
+            if (operation == '*')
+            {
+                var previous = parts[i - 1];
+                var next = parts[i + 1];
+                if (previous is double && next is double)
+                {
+                    Console.WriteLine($"previous: {previous}, next: {next},  operation: {operation}, answer: {(double)previous * (double)next}");
+                    parts[i] = (double)previous * (double)next;
+                    parts.Remove(previous);
+                    parts.Remove(next);
+                    i = i - 2;
+                }
+                else return new Result(1, $"Invalid order of operations at character {i + 1}", null);
+            }
+            if (operation == '/')
+            {
+                var previous = parts[i - 1];
+                var next = parts[i + 1];
+                if (previous is double && next is double)
+                {
+                    Console.WriteLine($"previous: {previous}, next: {next},  operation: {operation}, answer: {(double)previous / (double)next}");
+                    parts[i] = (double)previous / (double)next;
+                    parts.Remove(previous);
+                    parts.Remove(next);
+                    i = i - 2;
+                }
+                else return new Result(1, $"Invalid order of operations at character {i + 1}", null);
+            }
+        }
+
+    }
+    for (int i = 0; i < parts.Count; i++)
+    {
+        if (parts[i] is char operation)
+        {
+            if (operation == '+')
+            {
+                var previous = parts[i - 1];
+                var next = parts[i + 1];
+                if (previous is double && next is double)
+                {
+                    parts[i] = (double)previous + (double)next;
+                    parts.Remove(previous);
+                    parts.Remove(next);
+                    i = i - 2;
+                }
+                else return new Result(1, "Invalid order of operations", null);
+            }
+            if (operation == '-')
+            {
+                var previous = parts[i - 1];
+                var next = parts[i + 1];
+                if (previous is double && next is double)
+                {
+                    parts[i] = (double)previous - (double)next;
+                }
+                else return new Result(1, "Invalid order of operations", null);
+            }
+        }
+
+    }
+    return new Result(0, null, parts[0]);
 }
 
 void loop()
 {
-    string input = null;
+    string? input = null;
     while (input == null || input == "")
     {
         input = Console.ReadLine();
@@ -93,13 +179,33 @@ void loop()
     var res = handleInput(input);
     if (res.Status != 0)
     {
+        Console.WriteLine(res.Message);
         Console.WriteLine("Try again.");
         loop();
     }
     if (res.Data is not List<object>)
     {
-
+        Console.WriteLine("Something went wrong. Please try again.");
+        loop();
     }
+    Result processedRes = processParts((List<object>)res.Data!);
+    while (input == null || input == "")
+    {
+        input = Console.ReadLine();
+    }
+    if (processedRes.Status != 0)
+    {
+        Console.WriteLine(processedRes.Message);
+        Console.WriteLine("Try again.");
+        loop();
+    }
+    if (processedRes.Data is not double)
+    {
+        Console.WriteLine("Something went wrong. Please try again.");
+        loop();
+    }
+    Console.WriteLine((double)processedRes.Data);
+    loop();
 };
 
 
@@ -107,13 +213,13 @@ loop();
 
 public class Result
 {
-    public Result(int? status, string? message, dynamic? data)
+    public Result(double? status, string? message, dynamic? data)
     {
-        if (status != null) Status = (int)status;
+        if (status != null) Status = (double)status;
         if (message != null) Message = message;
         if (data != null) Data = data;
     }
-    public int Status { get; set; }
+    public double Status { get; set; }
     public string? Message { get; set; }
     public dynamic? Data { get; set; }
 }
